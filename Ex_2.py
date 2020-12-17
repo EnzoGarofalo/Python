7c40a6664364aaf9c1d07ccd4971c1b97ab2c8be
Enzo Garofalo <enzo.garofalo07@gmail,com>
2020-12-17 18:35:20 -0300

    update

--- Ex_2.py
+++ Ex_2.py
@@ -1,21 +1,21 @@
 # Implementar uma função que retorne verdadeiro se o número for primo
 # (falso caso contrário). Testar de 1 a 100.
 
-
-
-
 def  Éprimo(n):
 
-    for i in range(2, n):
-        if(n % i == 0):
+    if  n < 2:       
+        return False
+    
+    for i in (2,  n):
+        if  (n % i == 0):
             return False
         else:
             return True
-            
-n = 1
-for n in range(n, 101):
-    if Éprimo(n):
-        print ("Este Número: ", n, "é primo")
-    else:
-        print ("Este Número: ", n, " não é primo")   
-        
+
+    n = 1
+    for n in range(n, 101):
+        if Éprimo(n):
+            print ("Este Número: ", n, "é primo")
+        else:
+            print ("Este Número: ", n, " não é primo")   
+
---
+++ Ex_3.py
@@ -0,0 +1,9 @@
+#Imprima um Número
+# aprenda as diferenças entre as funções raw_input() e input()
+
+# usando o inputa funfa deboas, mas para versões de python antigos gera uma exeção NameErrror: "input"
+num =   input("Digite sua Idade: ")
+print ('Você tem', num, 'anos')
+#você resolve usando raw_input(), isso apenas para pythons de versões antigas
+# nome = raw_input('Digite o seu nome:')
+# print ('Olá', nome, 'Suerva?')
